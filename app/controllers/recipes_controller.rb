6b6813322f206e0e3f6fd1679d3e7dc45e9a66d5
Christopher Warner <c.warner1994@hotmail.co.uk>
2023-03-07 15:20:42 +0000

    recipes controller added with cisnedu methods

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,22 @@
+class RecipesController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
