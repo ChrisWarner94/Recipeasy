0fe2101fd05e8255d53da428dadba7d24f1a6350
Aisha Hersi <aishahersi509@gmail.com>
2023-03-07 17:50:34 +0000

    seed file done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :recipes do
+    match '/scrape', to: 'recipes#scrape', via: :post, on: :collection
+  end
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
