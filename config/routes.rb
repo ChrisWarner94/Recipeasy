168efde70515e5f208836257dd01303e7fbd52a4
Alia Eshaq <alia@greenbeeinsights.com>
2023-03-08 16:10:52 +0000

    updates to the new recipe pages inciuding addtions to controller to make it work

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
-  resources :recipes do
-    match '/scrape', to: 'recipes#scrape', via: :post, on: :collection
-  end
+  # resources :recipes do
+  #   match '/scrape', to: 'recipes#scrape', via: :post, on: :collection
+  # end
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'recipes/new1', to: 'recipes#new1'
   resources :recipes do
     resources :bookmarks, only: [:new, :create]
     resources  :favorites, only: [:show, :new, :create]
   end
-  get 'recipes/new1', to: 'recipes#new1'
+  get 'recipes/new2', to: 'recipes#new2'
 end
---
+++ db/migrate/20230308142303_addurl_to_recipe.rb
@@ -0,0 +1,5 @@
+class AddurlToRecipe < ActiveRecord::Migration[7.0]
+  def change
+      add_column :recipes, :url, :string
+  end
+end
